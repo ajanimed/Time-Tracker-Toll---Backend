1fd7d210db22af9370a6578a511e1f9b10f0fa39
ajanimed <ajani.med@gmail.com>
2018-11-25 16:53:55 +0300

    adding Swagger UI documentation

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ let bodyParser = require('body-parser');
 let connection = require('./config/db');
 let passport = require('passport');
 require('./Midllewears/passport');
+let swaggerUi = require('swagger-ui-express'),swaggerDocument = require('./openapi.json');
 
 
 let app = express();
 app.use(passport.initialize());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+//adding swagger ui route
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(require('./routes/auth'));
 let userRoute = require('./routes/users');
 app.use('/users', passport.authenticate('jwt', {session: false}), userRoute);
