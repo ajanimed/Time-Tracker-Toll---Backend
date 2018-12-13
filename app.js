a0463706ca82c32dbe9f016c2e71881c6649bb24
ajanimed <ajani.med@gmail.com>
2018-12-13 16:27:09 +0300

    Routes Implementation - wip

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let morgan = require('morgan');
+let winston = require('./config/winston');
 let express = require("express");
 let bodyParser = require('body-parser');
 let connection = require('./config/db');
@@ -7,17 +9,21 @@ let swaggerUi = require('swagger-ui-express'),swaggerDocument = require('./opena
 
 
 let app = express();
+//use morgan & winston logger
+app.use(morgan('combined', { stream: winston.stream }));
+//use authentification passport strategy
 app.use(passport.initialize());
+//using body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 //adding swagger ui route
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 //authentification route
 app.use(require('./routes/auth'));
 
 //protected routes
-
 let userRoute = require('./routes/users');
 let EmployeeRoute = require('./routes/employees');
 let SupervisorRoute = require('./routes/supervisors');
