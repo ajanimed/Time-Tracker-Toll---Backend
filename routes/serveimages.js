38da49e5bc92950c7c710cfb6e0b63baef6a21af
ajanimed <ajani.med@gmail.com>
2019-01-21 17:28:22 +0300

    Adding Heroku config - wip

--- routes/serveimages.js
+++ routes/serveimages.js
@@ -2,13 +2,13 @@ let express = require('express');
 let router = new express.Router();
 let config = require('config');
 //let root = config.get("servingProfileImagesDirectory");
-let root = path.dirname(require.main.filename);
+//let root = path.dirname(require.main.filename);
 
 //sending profile photos
 router.get('/profiles-photos/:userId/:photoName', function(req,res,next){
     //res.status(200).json({'yo':'yoyo'});
    let options = {
-       root: root+'/'+req.params.userId+'/',
+       root: __dirname+'/'+req.params.userId+'/',
        dotfiles: 'deny',
        headers: {
            'x-timestamp': Date.now(),
