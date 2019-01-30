cf168dfdefd491853410096c43ff2cc8ff9caf45
ajanimed <ajani.med@gmail.com>
2019-01-30 13:36:16 +0300

    Adding get route '/user/verifemail/:email'

--- Midllewears/verifmail.js
+++ Midllewears/verifmail.js
@@ -3,24 +3,11 @@ let Error = require("../Models/Error");
 
 Verifmail = function(req,res,next){
     if(req.body.email){
-        User.find({email:req.body.email}, (err,doc) =>{
-            if(err){
-                res.status(500).json(Error(500,err));
-            }
-            else{
-                if(doc.length!==0){
-                    res.status(500).json(Error.message(500,"Email adress already exist"));
-                }
-                else{
-                    next();
-                }
-            }
-        })
-    }
-    else{
-        if(req.params.email){
-            console.log(req.params.email);
-            User.find({email:req.params.email}, (err,doc) =>{
+        if(!req.body.email.includes('@')){
+            res.status(500).json(Error.message(500,"Not a valid mail"));
+        }
+        else{
+            User.find({email:req.body.email}, (err,doc) =>{
                 if(err){
                     res.status(500).json(Error(500,err));
                 }
@@ -29,10 +16,32 @@ Verifmail = function(req,res,next){
                         res.status(500).json(Error.message(500,"Email adress already exist"));
                     }
                     else{
-                        res.status(200).json(Error.message(200,"Email adress dosent exist"));
+                        next();
                     }
                 }
             })
+        }
+    }
+    else{
+        if(req.params.email){
+            if(!req.params.email.includes('@')){
+                res.status(500).json(Error.message(500,"Not a valid mail"));
+            }
+            else{
+                User.find({email:req.params.email}, (err,doc) =>{
+                    if(err){
+                        res.status(500).json(Error(500,err));
+                    }
+                    else{
+                        if(doc.length!==0){
+                            res.status(500).json(Error.message(500,"Email adress already exist"));
+                        }
+                        else{
+                            res.status(200).json(Error.message(200,"Email adress dosent exist"));
+                        }
+                    }
+                })
+            }
         }
     }
 }
